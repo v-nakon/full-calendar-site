3e5389bd27aaea4c4acdee2bb0e3ea910608e9be
v-nakon <nakonechny.vl@hotmail.com>
2020-04-07 14:02:19 +0300

    add create event on event page

--- app/js/event-page.js
+++ app/js/event-page.js
@@ -92,8 +92,12 @@ function setDate(obj) {
 }
 function setLocation(obj) {
   let location = obj.address;
+  let city = "";
+  if (obj.city !== null) {
+    city = obj.city.title + ", ";
+  }
   let locationElement = document.querySelector(".event_location_address");
-  locationElement.innerHTML = location;
+  locationElement.innerHTML = city + location;
 }
 function setPrice(obj) {
   let price = obj.cost;
