25413fe6fc27ce42ec57eaaba1eecfa308bb8e2a
v-nakon <nakonechny.vl@hotmail.com>
2020-04-10 16:00:06 +0300

    fix redirect time

--- app/js/redirect-page.js
+++ app/js/redirect-page.js
@@ -1,17 +1,18 @@
+import { getEvent } from "./helpers/requests.js";
+
 let urlStringParams = window.location.search;
 let urlParams = new URLSearchParams(urlStringParams);
 let idEvent = urlParams.get("id");
 
-axios
-  .get("https://eventafisha.com/api/v1/events/" + idEvent)
-  .then(function (response) {
-    document.title = response.data.title;
-    setTimeout(() => (document.location.href = response.data.buy_link), 4000);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
+getEventData(idEvent);
+
+function getEventData(idEvent) {
+  getEvent(idEvent)
+    .then((response) => {
+      document.title = response.data.title;
+      setTimeout(() => (document.location.href = response.data.buy_link), 2000);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
